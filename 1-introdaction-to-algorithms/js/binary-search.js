5ce900577c64c664c9f7a7790976b26b83a407e7
Zemliakov Konstantin <zemliakov.konstantin@gmail.com>
2019-01-27 19:34:29 +0600

    write binary-serarch algroithm in js

---
+++ 1-introdaction-to-algorithms/js/binary-search.js
@@ -0,0 +1,27 @@
+function binarySearch(list, item) {
+  let low = 0;
+  let high = list.length - 1;
+
+  let guess = list[mid];
+
+  while (high >= low) {
+    let mid = Math.floor((low + high) / 2);
+    let guess = list[mid];
+
+    if (guess === item) {
+      return mid;
+    }
+
+    if (guess > item) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+
+  return null;
+}
+
+const list = [1, 2, 3, 5];
+console.log(binarySearch(list, 3) === 2); // true
+console.log(binarySearch(list, 4) === null); // true
